85765e9fe8d65589872efb6e640f1d3627025206
gabardes <60473099+gabardes@users.noreply.github.com>
2022-04-13 16:26:49 -0300

    1st layout material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SendMedalComponent } from './send-medal/send-medal.component';
+import {MatCardModule} from '@angular/material/card';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { SendMedalComponent } from './send-medal/send-medal.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
